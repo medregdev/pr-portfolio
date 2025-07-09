ee43b8fb501bfc7698901b36ad2ebc92b064ce66
panharith <sethapanharith@gmail.com>
2025-07-09 14:40:14 +0700

    update logo with name

--- src/components/layout/HeroNavbar.vue
+++ src/components/layout/HeroNavbar.vue
@@ -1,7 +1,7 @@
 <template>
   <header>
     <div class="flex justify-between items-center p-8 lg:px-12 relative z-20">
-      <div class="text-3xl font-bold dark:text-white">LOGO</div>
+      <div class="text-3xl font-bold dark:text-white">Panharith</div>
       <!-- Mobile Toggle Button -->
       <div class="md:hidden z-30">
         <button class="block focus:outline-none" @click="isMenuOpen = !isMenuOpen">
